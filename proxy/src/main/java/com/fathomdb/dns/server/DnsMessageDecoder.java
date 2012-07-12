d4febd34b62277856c59006417c4c382f6e46349
Justin Santa Barbara <justin@fathomdb.com>
2012-07-11 20:38:08 -0700

    If we can't parse the DNS message, just swallow the exception

--- proxy/src/main/java/com/fathomdb/dns/server/DnsMessageDecoder.java
+++ proxy/src/main/java/com/fathomdb/dns/server/DnsMessageDecoder.java
@@ -4,9 +4,12 @@ import org.jboss.netty.buffer.ChannelBuffer;
 import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.handler.codec.oneone.OneToOneDecoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 //@Sharable
 public class DnsMessageDecoder extends OneToOneDecoder {
+	static final Logger log = LoggerFactory.getLogger(DnsMessageDecoder.class);
 
 	@Override
 	protected Object decode(ChannelHandlerContext ctx, Channel channel, Object msg) throws Exception {
@@ -14,19 +17,24 @@ public class DnsMessageDecoder extends OneToOneDecoder {
 			return msg;
 		}
 
-		ChannelBuffer channelBuffer = (ChannelBuffer) msg;
+		try {
+			ChannelBuffer channelBuffer = (ChannelBuffer) msg;
 
-		// Note - it sort of sucks that we have to copy the data
-		// Message is missing a (byte[], offset, length) constructor though,
-		// anyway
+			// Note - it sort of sucks that we have to copy the data
+			// Message is missing a (byte[], offset, length) constructor though,
+			// anyway
 
-		// TODO: Implement our own DNSInput?
+			// TODO: Implement our own DNSInput?
 
-		int length = channelBuffer.readableBytes();
-		byte[] messageData = new byte[length];
-		channelBuffer.readBytes(messageData);
+			int length = channelBuffer.readableBytes();
+			byte[] messageData = new byte[length];
+			channelBuffer.readBytes(messageData);
 
-		return new DnsMessage(messageData);
+			return new DnsMessage(messageData);
+		} catch (Exception e) {
+			log.warn("Error in DNS message decoder", e);
+			return null;
+		}
 	}
 
 }
