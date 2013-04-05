189873b0c9242e101e2d533e969cd08aafe884d3
Justin Santa Barbara <justin@fathomdb.com>
2013-04-05 15:02:54 -0700

    Add a parse method to HttpMethod

--- fathomdb-http/src/main/java/org/platformlayer/http/HttpMethod.java
+++ fathomdb-http/src/main/java/org/platformlayer/http/HttpMethod.java
@@ -1,9 +1,15 @@
 package org.platformlayer.http;
 
+import com.fathomdb.utils.EnumUtils;
+
 public enum HttpMethod {
-	GET, POST, PUT, DELETE;
+	GET, POST, PUT, DELETE, OPTIONS;
 
 	public String getHttpMethod() {
 		return name();
+	}
+
+	public static HttpMethod tryParse(String method) {
+		return EnumUtils.valueOfCaseInsensitive(HttpMethod.class, method);
 	}
 }
