46cfc7fea11d5794baeac2f3d7f721be652bd025
Justin Santa Barbara <justin@fathomdb.com>
2013-10-04 18:12:45 -0700

    Deprecate ByteSource (should use Guava)

--- fathomdb-commons/src/main/java/com/fathomdb/io/ByteSource.java
+++ fathomdb-commons/src/main/java/com/fathomdb/io/ByteSource.java
@@ -4,10 +4,12 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 
+@Deprecated
+// Prefer guava
 public interface ByteSource extends Closeable {
-	InputStream open() throws IOException;
+    InputStream open() throws IOException;
 
-	long getContentLength() throws IOException;
+    long getContentLength() throws IOException;
 
-	ByteMetadata getMetadata();
+    ByteMetadata getMetadata();
 }
