2f4ef7a02299948adac80e3db878983c34534953
Justin Santa Barbara <justin@fathomdb.com>
2013-10-05 17:35:40 -0700

    Update our JPA to latest guava

--- fathomdb-jpa/src/main/java/com/fathomdb/jpa/impl/ResultSetMappers.java
+++ fathomdb-jpa/src/main/java/com/fathomdb/jpa/impl/ResultSetMappers.java
@@ -1,67 +1,73 @@
 package com.fathomdb.jpa.impl;
 
 import java.util.Map;
-import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ExecutionException;
 
-import com.google.common.base.Function;
-import com.google.common.collect.MapMaker;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.google.common.collect.Maps;
 
 public class ResultSetMappers {
-	final DatabaseNameMapping databaseNameMapping;
+    final DatabaseNameMapping databaseNameMapping;
 
-	final Map<String, ResultSetMapper> sqlMappers = Maps.newHashMap();
-	final Map<String, Class<?>> classes = Maps.newHashMap();
+    final Map<String, ResultSetMapper> sqlMappers = Maps.newHashMap();
+    final Map<String, Class<?>> classes = Maps.newHashMap();
 
-	public ResultSetMappers(DatabaseNameMapping databaseMapping, Class<?>... modelClasses) {
-		super();
-		this.databaseNameMapping = databaseMapping;
-		addClass(modelClasses);
-	}
+    public ResultSetMappers(DatabaseNameMapping databaseMapping, Class<?>... modelClasses) {
+        super();
+        this.databaseNameMapping = databaseMapping;
+        addClass(modelClasses);
+    }
 
-	public void addClass(Class<?>... classes) {
-		for (Class<?> clazz : classes) {
-			String tableName = databaseNameMapping.getTableName(clazz);
-			this.classes.put(tableName, clazz);
-		}
-	}
+    public void addClass(Class<?>... classes) {
+        for (Class<?> clazz : classes) {
+            String tableName = databaseNameMapping.getTableName(clazz);
+            this.classes.put(tableName, clazz);
+        }
+    }
 
-	public ResultSetMapper get(String sql) {
-		ResultSetMapper resultSetMapper = sqlMappers.get(sql);
-		if (resultSetMapper == null) {
-			resultSetMapper = new ResultSetMapper(this);
-			sqlMappers.put(sql, resultSetMapper);
-		}
-		return resultSetMapper;
-	}
+    public ResultSetMapper get(String sql) {
+        ResultSetMapper resultSetMapper = sqlMappers.get(sql);
+        if (resultSetMapper == null) {
+            resultSetMapper = new ResultSetMapper(this);
+            sqlMappers.put(sql, resultSetMapper);
+        }
+        return resultSetMapper;
+    }
 
-	public Class<?> getClassForTableName(String tableName) {
-		return classes.get(tableName);
-	}
+    public Class<?> getClassForTableName(String tableName) {
+        return classes.get(tableName);
+    }
 
-	public DatabaseNameMapping getNameMapping() {
-		return this.databaseNameMapping;
-	}
+    public DatabaseNameMapping getNameMapping() {
+        return this.databaseNameMapping;
+    }
 
-	final ConcurrentMap<Class<?>, Relationships> relationshipsByClass = new MapMaker()
-			.makeComputingMap(new Function<Class<?>, Relationships>() {
-				@Override
-				public Relationships apply(Class<?> sourceClass) {
-					return buildRelationships(sourceClass);
-				}
-			});
+    final LoadingCache<Class<?>, Relationships> relationshipsByClass = CacheBuilder.newBuilder().build(
+            new CacheLoader<Class<?>, Relationships>() {
+                @Override
+                public Relationships load(Class<?> sourceClass) throws Exception {
+                    return buildRelationships(sourceClass);
+                }
+            });
 
-	public void populateInternalRelationships(JoinedQueryResult mapResult) throws SecurityException,
-			NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
-		for (Class<?> resultClass : mapResult.types.keySet()) {
-			Relationships relationships = relationshipsByClass.get(resultClass);
-			relationships.doMap(mapResult);
-		}
-	}
+    public void populateInternalRelationships(JoinedQueryResult mapResult) throws SecurityException,
+            NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+        for (Class<?> resultClass : mapResult.types.keySet()) {
+            Relationships relationships;
+            try {
+                relationships = relationshipsByClass.get(resultClass);
+            } catch (ExecutionException e) {
+                throw new IllegalStateException("Error building JPA relationship", e);
+            }
+            relationships.doMap(mapResult);
+        }
+    }
 
-	protected Relationships buildRelationships(Class<?> sourceClass) {
-		Relationships relationships = new Relationships(sourceClass);
+    protected Relationships buildRelationships(Class<?> sourceClass) {
+        Relationships relationships = new Relationships(sourceClass);
 
-		return relationships;
-	}
+        return relationships;
+    }
 }
